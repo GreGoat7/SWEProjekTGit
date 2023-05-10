5e45328056be1cfadbc8109f28e7dbecda23b66e
GreGoat7 <gregoridaiger@web.de>
2023-05-10 11:23:31 +0200

    Fitler Factory reflection idee noch hinzugefügt für Gespräch

--- src/main/java/filter/FilterFactory.java
+++ src/main/java/filter/FilterFactory.java
@@ -1,5 +1,11 @@
 package filter;
 
 public class FilterFactory {
-
+    // Erstellung der konkreten Filter-Objekte mit reflection
+    public static Filter createFilter(String filterClassName) throws Exception {
+        // Beispiel filterClassName = filter.converter.JsonToXmlConverter
+        Class<?> filterClass = Class.forName(filterClassName);
+        return (Filter) filterClass.getDeclaredConstructor().newInstance();
+    }
 }
+
